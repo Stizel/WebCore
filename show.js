270f7407a68e5e23a45ad654385f464fe9113e02
Stizel <79175842672@yandex.ru>
2022-09-05 17:42:57 +0300

    renaming show element to expand

--- show.js
+++ show.js
@@ -1,15 +1,15 @@
 
 let slider = document.querySelector('.slider');
-let show = document.querySelector('.brands__show');
+let show = document.querySelector('.brands__expand');
 show.addEventListener('click', evt => {
     evt.preventDefault();
     if (!slider.classList.contains('slider--open')){
         slider.classList.add('slider--open')
-        show.classList.add('close')
+        show.classList.add('brands__expand--close')
         show.innerHTML = 'Скрыть'
     } else {
         slider.classList.remove('slider--open')
-        show.classList.remove('close')
+        show.classList.remove('brands__expand--close')
         show.innerHTML = 'Показать все'
     }
 })
